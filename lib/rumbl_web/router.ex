c049540b6ef0346b724895886e1bec63ecc9f0f6
robinch <robinch88@gmail.com>
2020-03-13 11:17:04 +0100

    Chapter 5 done

---
+++ lib/rumbl_web/router.ex
@@ -0,0 +1,24 @@
+defmodule RumblWeb.Router do
+  use RumblWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug RumblWeb.Auth
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", RumblWeb do
+    # Use the default browser stack
+    pipe_through :browser
+    get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
+  end
+end
