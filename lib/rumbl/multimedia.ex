2863ef8e345d44552aaf53a5be81bfb463c8a97b
robinch <robinch88@gmail.com>
2020-03-13 16:11:33 +0100

    Chapter 6 done

---
+++ lib/rumbl/multimedia.ex
@@ -0,0 +1,53 @@
+defmodule Rumbl.Multimedia do
+  import Ecto.Query, warn: false
+  alias Rumbl.{Accounts, Repo}
+
+  alias Rumbl.Multimedia.{Category, Video}
+
+  def list_videos do
+    Repo.all(Video)
+  end
+
+  def list_user_videos(%Accounts.User{} = user) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.all()
+  end
+
+  def get_video!(id), do: Repo.get!(Video, id)
+
+  def get_user_video!(%Accounts.User{} = user, id) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.get!(id)
+  end
+
+  def create_video(%Accounts.User{} = user, attrs \\ %{}) do
+    %Video{}
+    |> Video.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:user, user)
+    |> Repo.insert()
+  end
+
+  def update_video(%Video{} = video, attrs) do
+    video
+    |> Video.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_video(%Video{} = video) do
+    Repo.delete(video)
+  end
+
+  def change_video(%Video{} = video) do
+    Video.changeset(video, %{})
+  end
+
+  def create_category!(name) do
+    Repo.insert!(%Category{name: name}, on_conflict: :nothing)
+  end
+
+  defp user_videos_query(query, %Accounts.User{id: user_id}) do
+    from(v in query, where: v.user_id == ^user_id)
+  end
+end
--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -1,5 +1,19 @@
 defmodule RumblWeb.Auth do
   import Plug.Conn
+  import Phoenix.Controller
+  alias RumblWeb.Router.Helpers, as: Routes
+
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
+  end
+
   def init(opts), do: opts
 
   def call(conn, _opts) do
